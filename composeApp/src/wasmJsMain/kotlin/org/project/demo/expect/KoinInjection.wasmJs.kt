aaf3be5af12642bc0ddfcf9a7d1a3c4963679616
taiebslama <slama.taieb@proxym-group.com>
2025-07-16 12:03:39 +0400

    first commit integrate koin + ktor

--- composeApp/src/wasmJsMain/kotlin/org/project/demo/expect/KoinInjection.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/org/project/demo/expect/KoinInjection.wasmJs.kt
@@ -1,6 +1,12 @@
 package org.project.demo.expect
 
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.js.Js
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
-actual val platformModules: Module = module {}
+actual val platformModules: Module = module {
+    single<HttpClientEngine> {
+        Js.create()
+    }
+}
